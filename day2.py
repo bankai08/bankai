525c02e0690485fdd5beb5a4eae32fed18611e91
Shinjo <168315561+bankai08@users.noreply.github.com>
2024-06-05 12:14:24 +0545

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,65 @@
+# a=int(input("Enter any number"))
+# b=input("Enter any number")
+# c="Bankai"
+# # print("The sum is",(a+int(b)))
+# print(f"HI {c} your sum is {a+int(b)}")
+
+
+
+# name="Bankai"
+# sub="python"
+# num1=8
+# num2=8
+# w="sleeping"
+# print(f"Hi This is my first time learning {sub}")
+# print(f"The sum of two numbers is {num1+num2}")
+# print(f"Hi This is {name} and I am {w}")
+
+#x=a^2+b^2
+# a=int(input("Enter a number"))
+# b=int(input("Enter second number"))
+# x=a**2+b**2
+# print(x)
+
+
+# y=a^30+b^40+3ab
+# a=int(input("ENter first number"))
+# b=int(input("ENter second number"))
+# y=a**30+b**40+3*a*b
+# print(y)
+
+
+#z=(x+y)^2+(x-y)^2
+# x=int(input("ENter first number"))
+# y=int(input("Enter second number"))
+# z=(x+y)**2+(x-y)**2
+# print(z)
+
+
+#c
+# a="4"
+# b="2"
+# print(f"Sum is {int(a)+int(b)}")
+# print(f"SUb is {int(a)-int(b)}")
+# print(f"Mul is {int(a)*int(b)}")
+# print(f"Div is {int(a)/int(b)}")
+
+
+#area of rectangle
+# l=int(input("ENter length"))
+# w=int(input("Enter width"))
+# A=l*w
+# print(A)
+
+
+#area of square
+# a=int(input("Enter length"))
+# A=a**2
+# print(A)
+
+
+
+#Area of circle
+r=int(input("Enter radius"))
+A=3.14*(r**2)
+print(A)
